a8ca9e61024ebf0dcfc7fe78e1c4236b14628d81
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-02-14 23:30:55 +0900

    백준 문자열 Complete

---
+++ 백준/단계별로풀어보기/문자열/Step10.java
@@ -0,0 +1,50 @@
+package Algorithms.백준.단계별로풀어보기.문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//1316 문제 그룹 단어 체커
+public class Step10 {
+
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception{
+
+        int count = 0;
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 0; i < N; i++) {
+            if (check() == true) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static boolean check() throws Exception {
+        boolean[] check = new boolean[26];
+        int prev = 0;
+        String str = bufferedReader.readLine();
+
+        for(int i = 0; i < str.length(); i++) {
+            int now = str.charAt(i);
+
+            if (prev != now) {
+
+                if ( check[now - 'a'] == false ) {
+                    check[now - 'a'] = true;
+                    prev = now;
+                }
+
+                else {
+                    return false;
+                }
+            }
+
+            else {
+                continue;
+            }
+        }
+        return true;
+    }
+}
---
+++ 백준/단계별로풀어보기/문자열/Step9.java
@@ -0,0 +1,54 @@
+package Algorithms.백준.단계별로풀어보기.문자열;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Step9 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+
+        int length = str.length();
+        int total = 0;
+
+        for (int i = 0; i < length; i++) {
+
+            char ch = str.charAt(i);
+
+            if(ch == 'c' && i < length - 1) {
+                if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-') {
+                    i++;
+                }
+            }
+
+            else if(ch == 'd' && i < length - 1) {
+                if(str.charAt(i + 1) == '-') {
+                    i++;
+                }
+                else if(str.charAt(i + 1) == 'z' && i < length - 2) {
+
+                    if(str.charAt(i + 2) == '=') {
+                        i += 2;
+                    }
+                }
+            }
+
+            else if((ch == 'l' || ch == 'n') && i < length - 1) {
+                if(str.charAt(i + 1) == 'j') {
+                    i++;
+                }
+            }
+
+
+            else if((ch == 's' || ch == 'z') && i < length - 1) {
+                if(str.charAt(i + 1) == '=') {
+                    i++;
+                }
+            }
+
+            total++;
+
+        }
+        System.out.print(total);
+    }
+}
