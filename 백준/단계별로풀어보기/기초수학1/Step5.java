774612a7611b84a6e0208e21e096da7e07f3e2a8
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-03-08 00:48:44 +0900

    백준 기초수학1 Step5 ACM호텔 Complete Step6 Start

--- 백준/단계별로풀어보기/기초수학1/Step5.java
+++ 백준/단계별로풀어보기/기초수학1/Step5.java
@@ -1,4 +1,52 @@
 package Algorithms.백준.단계별로풀어보기.기초수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 102520번 ACM호텔
 public class Step5 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int count = Integer.parseInt(bufferedReader.readLine());
+        String[] info;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < count; i++) {
+
+            info = bufferedReader.readLine().split(" ");
+
+            int floor = Integer.parseInt(info[0]);
+            int room = Integer.parseInt(info[1]);
+            int th = Integer.parseInt(info[2]);
+
+            int floorNumber = th % floor;
+            int roomNumber = th / floor + 1;
+
+            if(floorNumber == 0 ) {
+                floorNumber = floor;
+                roomNumber--;
+            }
+
+            if(i != 0 )
+                stringBuilder.append("\n");
+
+            stringBuilder.append(floorNumber);
+
+            if(roomNumber < 10) {
+                stringBuilder
+                        .append(0)
+                        .append(roomNumber);
+            }else {
+                stringBuilder.append(roomNumber);
+            }
+
+        }
+
+        System.out.print(stringBuilder);
+
+    }
 }
---
+++ 백준/단계별로풀어보기/기초수학1/Step6.java
@@ -0,0 +1,42 @@
+package Algorithms.백준.단계별로풀어보기.기초수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2775 부녀회장이 될테야
+public class Step6 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for(int i = 0; i < count; i++) {
+
+            int floor = Integer.parseInt(bufferedReader.readLine());
+            int room = Integer.parseInt(bufferedReader.readLine());
+
+            if(i != 0) {
+                stringBuilder.append("\n");
+            }
+
+            int tmp = 0;
+
+            if(room == 1) {
+                stringBuilder.append(1);
+            } else if (room == 2) {
+                stringBuilder.append(floor+1);
+            } else if(floor == 0) {
+                stringBuilder.append(room);
+            } else {
+                for(int j = 0; j < floor;)
+            }
+
+        }
+
+        System.out.print(stringBuilder);
+
+    }
+}
