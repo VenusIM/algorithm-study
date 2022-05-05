1001f1968b4b1968e68954799e97fc3ca1615fa5
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-05-05 23:45:49 +0900

    백준 동적계획법1 정수삼각형 Complete

--- 백준/단계별로풀어보기/동적계획법1/No_1003.java
+++ 백준/단계별로풀어보기/동적계획법1/No_1003.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Step1 {
+public class No_1003 {
 
     static int zero;
     static int one;
