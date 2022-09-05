61922e30d1bdcc447426f00385e2384be8d63f00
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-09-06 00:15:58 +0900

    백준 입출력 추가문제 Solve

--- 백준/단계별로풀어보기/입출력과사칙연산/No_3003.java
+++ 백준/단계별로풀어보기/입출력과사칙연산/No_3003.java
@@ -1,6 +1,8 @@
 package 백준.단계별로풀어보기.입출력과사칙연산;
 
+
 import java.io.*;
+import java.util.StringTokenizer;
 
 // 킹, 퀸, 룩, 비숍, 나이트, 폰
 public class No_3003 {
@@ -9,19 +11,21 @@ public class No_3003 {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str = bufferedReader.readLine();
-        char[] chess = {'1','1','2','2','2','8'};
+        int[] chess = {1,1,2,2,2,8};
+
+        StringTokenizer stringTokenizer = new StringTokenizer(str);
 
         int index = 0;
-        int checkNum = 0;
-        while(true) {
-            if(checkNum == 6) break;
-            bufferedWriter.write(chess[checkNum] - str.charAt(index));
-            bufferedWriter.write(" ");
-            checkNum++;
-            index += 2;
+        while (true){
+            bufferedWriter.write(String.valueOf(chess[index] - Integer.parseInt(stringTokenizer.nextToken())));
+            index++;
+            if(index == 6) {
+                break;
+            }else {
+                bufferedWriter.write(" ");
+            }
         }
-        bufferedReader.close();
         bufferedWriter.close();
+        bufferedReader.close();
     }
-
 }
