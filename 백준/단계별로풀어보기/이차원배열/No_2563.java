8ef412106a181cd734b530021d1e55c519b82863
yim3370 <71271289+yim3370@users.noreply.github.com>
2022-10-26 23:43:42 +0900

    2차원배열 색종이 solved

--- 백준/단계별로풀어보기/이차원배열/No_2563.java
+++ 백준/단계별로풀어보기/이차원배열/No_2563.java
@@ -3,18 +3,28 @@ package 백준.단계별로풀어보기.이차원배열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class No_2563 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-
-        int minX = 0;
-        int minY = 0;
-        int maxX = 0;
-        int maxY = 0;
-        for(int i = 0; i < n; i++) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = 0;  //검은 영역의 넓이
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[101][101];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        total++;
+                    }
+                }
+            }
         }
+        System.out.print(total);
     }
 }
