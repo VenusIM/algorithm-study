a41bf26ad3d17e09fda2582e52fb151f60b4847c
VenusIM <yim3370@gmail.com>
2025-02-18 00:09:21 +0900

    Codility Circle Rotation 100%

--- codility/CircleRotation.java
+++ codility/CircleRotation.java
@@ -1,29 +1,51 @@
 package Algorithms.codility;
 
+/**
+ * An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
+ * The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+ * Write a function:
+ * class Solution { public int[] solution(int[] A, int K); }
+ * that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+ *
+ * For example, given
+ *     A = [3, 8, 9, 7, 6]
+ *     K = 3
+ * the function should return [9, 7, 6, 3, 8].
+ * Three rotations were made:
+ *     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+ *     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+ *     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+ * For another example, given
+ *     A = [0, 0, 0]
+ *     K = 1
+ * the function should return [0, 0, 0]
+ * Given
+ *     A = [1, 2, 3, 4]
+ *     K = 4
+ * the function should return [1, 2, 3, 4]
+ *
+ * Assume that:
+ * N and K are integers within the range [0..100];
+ * each element of array A is an integer within the range [−1,000..1,000].
+ */
+
 public class CircleRotation {
     public static void main(String[] args) {
         new CircleRotation().solution(new int[]{3, 8, 9, 7, 6}, 3);
     }
 
     public int[] solution(int[] A, int K) {
-        // Implement your solution here
 
         int size = A.length;
 
-        int moveCount = K%size;
-        if(moveCount == 0) {
+        if (size == 0 || K % size == 0) {
             return A;
         }
 
         int[] result = new int[size];
 
-        K--;
-        for(int i = K; i < size; i++) {
-            result[i - K] = A[i];
-        }
-
-        for(int i = 0; i < K; i++) {
-            result[K + 1 + i] = A[i];
+        for (int i = 0; i < size; i++) {
+            result[(i + K) % size] = A[i];
         }
 
         return result;
